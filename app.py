dde2492e1af097460418200a9bd157d6e2e16e23
Irvin Avitia <97401201+ia-usgs@users.noreply.github.com>
2024-04-11 09:08:14 -0700

    test

--- app.py
+++ app.py
@@ -1,14 +1,53 @@
-from flask import Flask, render_template
 import os
 
+from flask import Flask, jsonify, render_template, redirect, request, url_for
+from flask_sqlalchemy import SQLAlchemy
+from keycloak import KeycloakOpenID
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://admin:kali@localhost/irvindb'
+db = SQLAlchemy(app)
+
+keycloak_client = KeycloakOpenID(
+    server_url="https://pacer-dev.northgrum.com",
+    client_id="pacer-dev",
+    realm_name="developer",
+    client_secret_key="redhatsso"
+)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+
 
 @app.route('/')
-def hello():
-    return render_template('index.html')
+def index():
+    return render_template('login.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/keycloak-login')
+def keycloak_login():
+    redirect_url = url_for('authorize', _external=True).decode('utf-8')
+    return redirect(keycloak_client.auth_url(redirect_uri=redirect_url))
+
+
+@app.route('/authorize', methods=['GET'])
+def authorize():
+    code = request.args.get('code')
+    token = keycloak_client.token(code=code)
+    user_info = keycloak_client.userinfo(token['access_token'])
+    username = user_info['preferred_username']
+    user_id = int(ord(username[0]) - ord('0'))
+    return jsonify({'user_id': user_id})
+
 
 if __name__ == '__main__':
-    port = os.environ.get('FLASK_PORT') or 8080
-    port = int(port)
+    port = int(os.environ.get('FLASK_PORT', 8080))
+    app.run(port=port, host='0.0.0.0')
 
-    app.run(port=port,host='0.0.0.0', debug=True)
