e35f3ad994e2a7ed55bb1c1b855c7f68c12f7585
Irvin Avitia <97401201+ia-usgs@users.noreply.github.com>
2024-04-11 07:25:34 -0700

    added index.html to route

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    return app.send_static_file('index.html')
 
 if __name__ == '__main__':
     port = os.environ.get('FLASK_PORT') or 8080
     port = int(port)
 
-    app.run(port=port,host='0.0.0.0')
+    app.run(port=port,host='0.0.0.0', debug=True)
