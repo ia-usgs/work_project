b2f868126cd8ff71557fab5bf5e3e3a80c60c835
Irvin Avitia <97401201+ia-usgs@users.noreply.github.com>
2024-04-11 07:12:13 -0700

    updated

--- app.py
+++ app.py
@@ -1,21 +1,14 @@
-from flask import Flask, render_template, request, make_response, current_app
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import inspect
-from flask_caching import Cache
+from flask import Flask
+import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root@localhost:3306/used_car_database'
-app.config['CACHE_TYPE'] = 'simple'
-app.config['CACHE_DEFAULT_TIMEOUT'] = 300
-db = SQLAlchemy(app)
-cache = Cache(app)
 
 @app.route('/')
-@cache.cached(timeout=300)
-def index():
-    inspector = inspect(db.engine)
-    tables = inspector.get_table_names()
-    return render_template('index.html', tables=tables)
+def hello():
+    return "Hello World!"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = os.environ.get('FLASK_PORT') or 8080
+    port = int(port)
+
+    app.run(port=port,host='0.0.0.0')
