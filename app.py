5e9b4a570027ec1c03225ad189e44c2208674406
Irvin Avitia <97401201+ia-usgs@users.noreply.github.com>
2024-04-11 07:36:46 -0700

    .

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return app.send_static_file('index.html')
+    return "hello worlddddddddd"
 
 if __name__ == '__main__':
     port = os.environ.get('FLASK_PORT') or 8080
