65d373daf6b72c3fdcc56e60dbda5614173ba826
Irvin Avitia <97401201+ia-usgs@users.noreply.github.com>
2024-04-17 08:14:13 -0700

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,8 @@ db = SQLAlchemy(app)
 keycloak_client = KeycloakOpenID(
     server_url="https://keycloak-ekho-sso-dev.apps.ocpshareddev.gc1.myngc.com/auth/",
     client_id="pacer-dev",
-    realm_name="developer"
+    realm_name="developer", 
+    verify=False
 )
 
 
